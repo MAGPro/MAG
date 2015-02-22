eaf2a6e6f3a2ef23e0275a0897ca8f653b7c9027
Gabriela <gabicollares@gmail.com>
2015-02-22 17:36:35 -0300

    Pacote Venda
    Adição de atributos em Venda e adicao de ContasReceber.

---
+++ src/br/com/mag/mb/ContasReceberMB.java
@@ -0,0 +1,5 @@
+package br.com.mag.mb;
+
+public class ContasReceberMB {
+
+}
--- src/br/com/mag/mb/VendaMB.java
+++ src/br/com/mag/mb/VendaMB.java
@@ -1,84 +1,178 @@
-package 
-br.com.mag.mb;
+package br.com.mag.mb;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.swing.text.html.ListView;
+
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.CellEditEvent;
+import org.primefaces.event.RowEditEvent;
 
 import br.com.mag.business.Cliente;
 import br.com.mag.business.ItemVenda;
 import br.com.mag.business.Produto;
 import br.com.mag.business.Venda;
 import br.com.mag.business.dao.ClienteDAO;
-import br.com.mag.business.dao.ItemVendaDAO;
+import br.com.mag.business.dao.DAOException;
 import br.com.mag.business.dao.ProdutoDAO;
 import br.com.mag.business.dao.VendaDAO;
 import br.com.mag.business.enumeration.TipoFormaPagamento;
 
 @ManagedBean
-@ViewScoped
-public class VendaMB{
-	
+@SessionScoped
+public class VendaMB {
+
 	private ItemVenda item = new ItemVenda();
 	private Venda venda = new Venda();
-	private Integer idProduto;
 	private double valorTotalVenda;
 	private double valorTotalCobrar;
+	private Integer idProduto;
 	private Integer idCliente;
 	private List<Venda> vendas;
 	private double qtdEstoqueAtualizado;
 	private Produto produto;
+	private VendaDAO vendaDAO = new VendaDAO();
+
 	
-	public double getTotalVenda(){
-			valorTotalVenda+=item.getTotal()-venda.getDesconto();;
-		return valorTotalVenda;	
+	
+	/*
+	 public List<Produto> completeProduto(String descricao) {
+	        return  produtoDAO.buscaPelaDescricao(descricao); 
+	    }
+	   */
+	public double getTotalVenda() {
+		valorTotalVenda += item.getTotal();
+		venda.setValorTotal(valorTotalVenda);
+		return valorTotalVenda;
 	}
-	public double getTotalCobrar(){
-			valorTotalCobrar = valorTotalVenda-venda.getDesconto();
+
+	public double getTotalCobrar() {
+		valorTotalCobrar = valorTotalVenda - venda.getDesconto();
+		System.out.println(valorTotalVenda + "-" + venda.getDesconto()
+				+ "valor a cobrar " + valorTotalCobrar);
+		venda.setValorTotalCobrar(valorTotalCobrar);
 		return valorTotalCobrar;
 	}
-	
-	public void adicionar() {
-		VendaDAO vendaDAO = new VendaDAO();
-		ItemVendaDAO itemDAO = new ItemVendaDAO();	
-		vendaDAO.salvar(venda);
-		atualizaEstoque(produto);
-		
-		for (ItemVenda item : venda.getItens()) {
-			itemDAO.salvar(item);
+
+	public void atualizaEstoque(Produto produto) {
+		qtdEstoqueAtualizado = item.getProduto().getQtdEstoque()
+				- item.getQuantidade();
+		System.out.println(qtdEstoqueAtualizado);
+		produto.setQtdEstoque(qtdEstoqueAtualizado);
+	}
+
+	public String salvar() {
+		RequestContext context = RequestContext.getCurrentInstance();
+		try {
+			if (venda == null) {
+
+			} else if (venda.getCodigoVenda() == null) {
+				ProdutoDAO produtoDAO = new ProdutoDAO();
+				vendaDAO.salvar(venda);
+
+				for (ItemVenda item : venda.getItens()) {
+					produtoDAO.atualizaEstoque(item.getProduto());
+					System.out.println(item.getProduto().getDescricao());
+				}
+				venda = null;
+				context.execute("confirmation.show();");
+				
+			} else {
+				if (vendaDAO.getPrimaryKey(venda) == null) {
+					throw new Exception(
+							"Erro ao atualizar, registro não encontrado");
+				}
+
+				vendaDAO.editar(venda);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
-		this.venda = new Venda();
+		return "/buscaVenda.faces";
 	}
-	public void atualizaEstoque(Produto produto){
-		qtdEstoqueAtualizado = produto.getQtdEstoque()-item.getQuantidade();
-		System.out.println(produto.getQtdEstoque());
-		
+
+	public String voltar() {
+
+		return "/buscaVenda.faces";
 	}
+
+
+
 	public void adicionarItem() {
 		ProdutoDAO produtoDAO = new ProdutoDAO();
 		ClienteDAO clienteDAO = new ClienteDAO();
-		
-		//Carregando dados do produto
+
+		// Carregando dados do produto
 		produto = produtoDAO.getPrimaryKey(idProduto);
-		//carregando dados do cliente
+		// carregando dados do cliente
 		Cliente cliente = clienteDAO.getPrimaryKey(idCliente);
-		
+
 		item.setProduto(produto);
 		item.setValorVenda(produto.getValorVenda());
-		
-		//Mapeamento bidirecional
+
+		// bidirecional
 		item.setVenda(venda);
 		venda.adicionarItem(item);
-		
-		
+
+		atualizaEstoque(produto);
 		venda.setCliente(cliente);
 		venda.setValorTotal(getTotalVenda());
-		//venda.setValorTotalCobrar(getValorTotalCobrar());
 
-		
 		item = new ItemVenda();
+	}
+	
+	public String editar() throws DAOException {
+
+		venda = vendaDAO.getPrimaryKey(venda);
+		return "/cadastraVenda.faces";
+	}
+
+	public String cadastrar() {
+
+		return "/cadastraVenda.faces?faces-redirect=true";
+	}
+
+	public String visualizar() throws DAOException {
+
+		venda = vendaDAO.getPrimaryKey(venda);
+
+		return "/visualizaVenda.faces";
+	}
+	public void setItem(ItemVenda item) {
+		this.item = item;
+	}
+
+	public void setVenda(Venda venda) {
+		this.venda = venda;
+	}
+	
+	public List<Venda> getVendas() {
+		if (vendas == null) {
+			vendas = new ArrayList<Venda>();
+		}
+		if (vendas.isEmpty()) {
+			try {
+				VendaDAO vendaDAO = new VendaDAO();
+				List<Venda> vendaList = vendaDAO.listarTodos();
+				for (Venda venda : vendaList) {
+					vendas.add(venda);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return vendas;
+	}
+	public void setVendas(List<Venda> vendas) {
+		this.vendas = vendas;
 	}
 
 	public ItemVenda getItem() {
@@ -96,38 +190,27 @@ public class VendaMB{
 	public Venda getVenda() {
 		return venda;
 	}
-	
-	public TipoFormaPagamento[] getTipos(){
+
+	public TipoFormaPagamento[] getTipos() {
 		return TipoFormaPagamento.values();
 	}
-	
-	public Integer getIdCliente(){
+
+	public Integer getIdCliente() {
 		return idCliente;
 	}
-	
-	public void setIdCliente(Integer idCliente){
+
+	public void setIdCliente(Integer idCliente) {
 		this.idCliente = idCliente;
 	}
-	public List<Venda> getVendas() {
-		if (vendas == null) {
-			vendas = new ArrayList<Venda>();
-		}
 
-		if (vendas.isEmpty()) {
-			try {
-				VendaDAO vendaDAO =  new VendaDAO();
-				List<Venda> vendaList = vendaDAO.listarTodos();
-				for (Venda venda : vendaList) {
-					vendas.add(venda);
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return vendas;
+	public double getQtdEstoqueAtualizado() {
+		return qtdEstoqueAtualizado;
 	}
-	public void setVendas(List<Venda> vendas) {
-		this.vendas = vendas;
+
+	public void setQtdEstoqueAtualizado(double qtdEstoqueAtualizado) {
+		this.qtdEstoqueAtualizado = qtdEstoqueAtualizado;
 	}
+
 	
+
 }
